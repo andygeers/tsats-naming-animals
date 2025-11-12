4f1e46ed61208c175560195672ab0bdd4333004b
Andy Geers <andy@geero.net>
2025-11-12 17:09:46 +0000

    Add in some placeholders for missing entries in French

--- Assets/Tests/FrenchNameBuilderTest.cs
+++ Assets/Tests/FrenchNameBuilderTest.cs
@@ -22,12 +22,21 @@ public class FrenchNameBuilderTest : BaseNameBuilderTest
     [TestCase("Fish", new string[] { "Nageoire", "Doré" }, "Nageoire dorée")]   // ...
     [TestCase("Fish", new string[] { "Écailles", "Doré" }, "Écailles dorées")]   // Plurals
     [TestCase("Fish", new string[] { "Écailles", "Tacheté" }, "Écailles tachetées")]   // ...
-    [TestCase("Fish", new string[] { "Nageoire", "Blanc", "Poisson" }, "Poisson à la nageoires blanche")]   // A name and two adjectives
+    [TestCase("Fish", new string[] { "Nageoire", "Blanc", "Poisson" }, "Poisson aux nageoires blanche")]   // A name and two adjectives
     [TestCase("Fish", new string[] { "Poisson", "Doré", "Dos" }, "Poisson à dos doré")]
     [TestCase("Fish", new string[] { "Poisson", "Écailles", "Tacheté" }, "Poisson aux écailles tachetées")]
     [TestCase("Fish", new string[] { "Tacheté", "Doré", "Blanc" }, "Taches dorées et blanches")] // Three adjectives
     [TestCase("Fish", new string[] { "Doré", "Tacheté", "Blanc" }, "Or tacheté et blanc")] // Three adjectives
     [TestCase("Fish", new string[] { "Blanc", "Doré", "Tacheté" }, "Truc blanc doré et tacheté")] // Three adjectives
+
+    [TestCase("Fish", new string[] { "Nageoire", "Écailles" }, "Nageoire aux écailles")] // Not checked with French native
+    [TestCase("Fish", new string[] { "Nageoire", "Écailles", "Doré" }, "Nageoire aux écailles et dos")] // Not checked with French native
+    [TestCase("Fish", new string[] { "Dos", "Nageoire", "Écailles" }, "Dos aux nageoires et écailles")] // Not checked with French native
+
+    // TODO:
+    // What is the equivalent of "Water dweller", "Water dwelling fish", and "Gold water dwelling fish"?
+    // Similarly, "Teemer", "Teeming fish"
+    // And "Dreamer", "Dreaming carp"
     public void FrenchNameBuilderFish(string animalName, string[] inputWords, string expectedName)
     {
         TestNameCombo(animalName, inputWords, expectedName);
